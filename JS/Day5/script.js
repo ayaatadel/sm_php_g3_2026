e44c9f295a7b818ec4e7c9cd348e33505df653a0
ayaatadel <ayaatadel128@gmail.com>
2026-09-01 15:22:29 +0300

    promise

--- JS/Day5/script.js
+++ JS/Day5/script.js
@@ -1,423 +1,287 @@
-/**
- * DOM : Document object Model 
- * 
- * == document ---> control html elemnts
- * 
- * Select : (getElement)
- * element: id , class ,tag name , name , (query selector , query selector all) ==> like css selectors
- * public:  body , images , title , forms , links 
-
-*/
-
-// var mainText=document.getElementById("pg") // element
-// class :  HTMLCollection == array
-// var mainText=document.getElementsByClassName("text") // element
-// var mainText=document.getElementsByTagName("p") // HTMLCollection
-// var mainText=document.querySelector("#pg") // first element ==> node
-// var mainText=document.querySelectorAll(".text") //node list ==> array
-// console.log(mainText);
-// var mainText=document.getElementsByTagName("p") // HTMLCollection
-
-//======= public selectors
-document.title = "Day5 js";
-
-// console.log(document.forms);
-// console.log(document.body);
-
-// console.log(document.images[0]);
-
-/**
- * innerText : text
- * textContent  : text
- * innerHTML : html code
- */
-
-// var mainText=document.querySelector("#pg2")
-// mainText.innerText="test"
-// mainText.innerHTML=`
-//   <img src="../Day4/imgs/Js_Run_time.png" alt=""  width="200px" height="200px"/>
-//  <span>
-
-//   Lorem ipsum dolor sit amet con
-//  </span>
-
-//  <button> BUY</button>
-
-// `
-
-// mainText.textContent="<p> lorem </p>"
-// console.log(mainText);
-
-// // ================ style ======
-// var mainText=document.querySelector("#pg2")
-
-// mainText.style.color="red";
-// mainText.style.border="1px solid black"
-// mainText.style.padding="15px";
-
-// // ``  : ذ
-// mainText.style.cssText= `
-// color:red;
-// border : 1px solid black;
-// padding: 15px;
-// text-align:center
-// `
-
-// var bttn=document.getElementById("btn");
-
-// bttn.style.cssText=`
-// border-radius:5px;
-// border:none;
-// background-color:brown;
-// color:white;
-// padding:5px;
-// text-align:center
-
-// `
-
-//========== classes ===
-/**
- * add class
- * remove class
- * check class exist or not : contains =>(true : class exist) , (false : class not exist)
- * toggle : class exist (remove) , class not exist (add)
- */
-
-// var mainText=document.getElementById("pg")
-// // console.log(mainText);
-// mainText.classList.add("text-contain","text-info")
-
-// mainText.classList.remove("text-info")
-// // mainText.classList.toggle("text-info") // add
-
-// console.log(mainText.classList.contains("text-info"));
-
-// =========== attributes
-/**
- * public attributes : class , id , width , heigth
- * element attributes  : arrributes specific for element
- *
- * img : alt , src  , title
- * link : href
- * input : name , placeholders
- * label :for
- * form : method , action
- *
- *
- * ==========
- * get attribute
- * set attribute  --> (add , update)
- * remove attribute
- *
- */
-
-// var img=document.getElementById("img1") // element selector
-// // console.log(img);
-
-// // var img1=(document.images)[0] // public selector
-// // console.log(img1);
-
-// //======== Get Attribute  ==> element.attribute || element.getAttribute('attribute_name')
-
-// console.log(img.src);
-// console.log(img.getAttribute("alt")
-// );
-
-// //======= Add Attribute || Update value of attribute ==> element.attribute=value ||  element.setAttribute('atr_name','value')
-// // img.title="code";
-// img.setAttribute("title","img code ")
-
-// //========= remove attribute
-// img.removeAttribute("title")
-
-//=================== create element
-
-// var cardsContainer=(document.getElementsByClassName("cards"))[0];
-// cardsContainer.innerHTML=`
-//  <h2>Expresso drinks</h2>
-//       <p>Lorem ipsum dolor sit amet.</p>
-
-//       <section class="container">
-//         <section class="card">
-//           <img src="./1.jfif" alt="code" />
-//                   <span> 5.30$</span>
-//           <p>
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-//             nostrum vero?
-//           </p>
-//         </section>
-//         <section class="card">
-//           <img src="./1.jfif" alt="code" />
-//                   <span> 5.30$</span>
-//           <p>
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-//             nostrum vero?
-//           </p>
-//         </section>
-//         <section class="card">
-//           <img src="./1.jfif" alt="code" />
-//                   <span> 5.30$</span>
-//           <p>
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-//             nostrum vero?
-//           </p>
-//         </section>
-//       </section>
-//     </section>
-
-// --------------------------------- create element ---------
-
-// var cardsContainer=document.createElement("section")
-// cardsContainer.classList.add("cards")
-// // var cardsContainer = document.getElementsByClassName("cards")[0];
-
-// var container = document.createElement("section");
-// container.classList.add("container");
-// var cardHeader = document.createElement("h2");
-// var cardText = document.createElement("p");
-// cardHeader.innerText = "Expresso Drinks";
-
-// cardText.innerText = "Lorem ipsum dolor sit amet.";
-
-//             nostrum vero?"
-
-// append child : add one child
-// append add more than one child
-// document.body.appendChild(cardsContainer);
-// cardsContainer.appendChild(container);
-// container.append(cardHeader, cardText);
-// var card;
-
-// function createCard(img, title, text) {
-//   card = document.createElement("section");
-//   card.classList.add("card");
-
-//   var cardimg = document.createElement("img");
-//   cardimg.src = img;
-//   var cardPrice = document.createElement("span");
-//   cardPrice.innerText = title;
-//   var cardTitle = document.createElement("p");
-//   cardTitle.innerText = text;
-//   card.append(cardimg, cardPrice, cardTitle);
-//   container.append( card);
-// }
-// createCard(
-//   "./1.jfif",
-//   "5.30$",
-//   " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-// );
-// createCard(
-//     "./1.jfif",
-//     "5.30$",
-//     " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-// );
-// createCard(
-//     "./1.jfif",
-//     "5.30$",
-//     " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-// );
-
-
-// var products=[
-//     {
-//         img:"./1.jfif",
-//         price:"5.30$",
-//         title:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-
-//     },
-//     {
-//         img:"./1.jfif",
-//         price:"5.30$",
-//         title:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-
-//     },
-//     {
-//         img:"./1.jfif",
-//         price:"5.30$",
-//         title:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-
-//     },
-//     {
-//         img:"./1.jfif",
-//         price:"5.30$",
-//         title:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-
-//     },
-//     {
-//         img:"./1.jfif",
-//         price:"5.30$",
-//         title:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-
-//     },
-//     {
-//         img:"./1.jfif",
-//         price:"5.30$",
-//         title:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-
-//     },
-//     {
-//         img:"./1.jfif",
-//         price:"5.30$",
-//         title:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam",
-
-//     },
-// ]
-
-// products.forEach((product)=>{
-//     // console.log(product);
-
-//     createCard(product.img,product.price,product.title)
-
-// })
-
-
-
-//=============== remove elemment ======
-/**
- * create 
- * select 
- */
-// Element.remove()
-// cardsContainer.remove()
-
-
-
-// ========== Event ============
-
-/**
- * Action :  execute 
- * 
- * onEvent  ==> onClick  : execute on event 
- * addEventListener   ==> addEventListener('click')==> can execute more than event on same element
- * 
- * 
- */
-
-// var bttn=document.getElementById("btn");
-
-// bttn.onclick=()=>{
-//     console.log("hello");
-    
-// }
-
-
-
-// bttn.onclick=function()
-// {
-//     console.log("php");
-    
-// }
-
-
-// bttn.addEventListener('click',function (){
-//      console.log("hello");
-    
-// })
-// bttn.addEventListener('click',()=>{
-// console.log("php");
-// })
-
-
-
-// bttn.addEventListener('click',print)
-// function print()
-// {
-//     console.log("print");
-    
-// }
-
-// var result=()=>{
-//     console.log("result");
-    
-// }
-// bttn.addEventListener('click',result)
-
-// //============= ex1
-// var bttn=document.getElementById("btn");
-// var inputTex=document.getElementById("text");
-// var outText=document.getElementById("output");
-
-
-
-
-
-// bttn.addEventListener('click',function (){
-
-//    outText.innerText=inputTex.value; 
-// })
-
-
-
-
-//============= ex2
-// var bttn=document.getElementById("btn");
-// var inputTex=document.getElementById("text");
-// var outText=document.getElementById("output");
-
-
-
-// // inputTex.addEventListener('keydown',(e)=>{
-// // // outText.innerText=inputTex.value; 
-// // // console.log(e.key);
-// // console.log(e.code);
-
-// // })
-// // // inputTex.addEventListener('keyup',()=>{
-// // // outText.innerText=inputTex.value; 
-// // // })
-
-// // var formData=document.getElementById("form");
-
-// // bttn.addEventListener('click',(e)=>{
-// //     e.preventDefault();
-// // console.log(e);
-// // console.log(e.target);
-
-// // })
-
-
-// //=============== BOM : Browser Object Model ===
+// //=============== get data  from api ======
 // /**
-//  * object : browser 
-//  * window object ===> window
-//  */
+//  * API : Application Programming Interface
+//  *
+//  * link ===> back end ==> front
+//  *          api
+//  * laravel ====> mobile
+//  *
+//  * link : data ===> json
+//  *
+//  *json  :{
+//  * "key":"value"
+//  * }
+//  *             (url)
+//  *  resquest  =======> server
+//  *
+//  *  response <========
+//  *
+//  * check ===> request already sent
+//  *
+//  * request:
+//  *
+//  * check ===> reponse (data) already retrived
+//  *
+//  *
+// //  * hrh : xml http request
+// //  *
+// //  *
+// //  */
+// // // var allproducts;
+// // // var xhr = new XMLHttpRequest(); //  0
+// // // try {
+// // //   xhr.open("get", "https://dummyjson.com/products"); //1
+// // //   var sendRequest = xhr.send(); //2
+// // //   // console.log(sendRequest);
 
-// var btnOpen =document.getElementById("openBtn");
-// var btnClose =document.getElementById("closeBtn");
-// var btnResizeTo =document.getElementById("resizeToBtn");
-// var btnResizeBy =document.getElementById("resizeByBtn");
-// var moveToBtn =document.getElementById("moveToBtn");
-// var moveByBtn =document.getElementById("moveByBtn");
+// // //   // check status request
 
-// var win;
-// btnOpen.addEventListener('click',()=>{
-// win=window.open("./about.html","_blank","width=100;height=100")
-// })
-// btnClose.addEventListener('click',()=>{
-//     win.close()
-// })
-// btnResizeTo.addEventListener('click',()=>{
-//     win.focus();
-//     win.resizeTo(200,200)
-// })
-// btnResizeBy.addEventListener('click',()=>{
-//     win.focus();
-//     win.resizeBy(100,100)
-// })
-// moveToBtn.addEventListener('click',()=>{
-//     win.focus();
-//     win.moveTo(100,100)
-// })
-// moveByBtn.addEventListener('click',()=>{
-//     win.focus();
-//     win.moveBy(100,100)
+// // //   xhr.onreadystatechange = () => {
+// // //     if (xhr.readyState == 4) /// done
+// // //     {
+// // //       // response
+// // //       if (xhr.status == 200) // data
+// // //       {
+// // //         // var data=xhr.response // string
+// // //         // console.log(typeof(data));
+
+// // //         // console.log(data);
+
+// // //         var data = JSON.parse(xhr.response); // ===> convert json string to json object
+// // //         //  console.log(typeof(data));
+// // //         // console.log(data);// object ===> key:products ==> []
+// // //         allproducts = data.products;
+// // //         //   products.forEach(product => {
+// // //         //     // console.log(product);  // create card
+
+// // //         //   });
+// // //         ////////////////////
+// // //         // let promise = new Promise(function (resolve, reject) {
+// // //         //   resolve(allproducts);
+// // //         //   // reject("error")
+// // //         //   // resolve();
+// // //         // });
+// // //         // //  console.log(promise);
+
+// // //         // promise
+// // //         //   .then((info) => {
+// // //         //     console.log(info);
+// // //         //   })
+// // //         //   .catch(() => {
+// // //         //     console.log("erroor");
+// // //         //   });
+
+// // //         ///////////////////////////////
+// // //       }
+// // //     }
+// // //   };
+// // // } catch (error) {
+// // //   console.log(error);
+// // // }
+
+// // // call back hell ===> promise : object ((resolve),(reject))
+// // /**
+// //  * <fulfilled>: 'success'
+// //  * <pending>: 'pending'
+// //  * <reject>: 'error'
+// //  */
+
+// // var users = [
+// //   {
+// //     id: 1,
+// //     name: "iti",
+// //   },
+// //   {
+// //     id: 1,
+// //     name: "iti",
+// //   },
+// //   {
+// //     id: 1,
+// //     name: "iti",
+// //   },
+// //   {
+// //     id: 1,
+// //     name: "iti",
+// //   },
+// // ];
+// // let promise = new Promise(function (resolve, reject) {
+// // code
+// //   resolve(users);
+// //   // reject("error")
+// //   // resolve();
+// // });
+// // //  console.log(promise);
+
+// // promise
+// //   .then((info) => {
+// //     return info
+// //   })
+// //   .catch(() => {
+// //     console.log("erroor");
+// //   });
+
+// // var users = [
+// //   {
+// //     id: 1,
+// //     name: "iti",
+// //   },
+// //   {
+// //    id: 2,
+// //     name: "iti",
+// //   },
+// //   {
+// //    id: 3,
+// //     name: "iti",
+// //   },
+// //   {
+// //    id: 4,
+// //     name: "iti",
+// //   },
+// // ];
+
+// // let promise = new Promise(function (resolve, reject) {
+// //  var user={
+// //     id:userId,
+// //     name:"iti",
+// //  }
+
+// // // });var users = [
+// // function getUserId(id) {
+// //     // var promise =new Promise ((resolve,reject)=>{
+// // //   const user = users.find((u) => u.id === id);
+// // //     // var user=users.find((user)=>{
+// // //     // if(user.id==id)
+// // //     // {
+// // //     //     return user
+// // //     // }
+// // //     // })
+// // //     if (user) {
+// // //       resolve(user);
+// // //     } else {
+// // //       reject("User not found");
+// // //     }
+// //     // })
+// //     // return promise;
+
+// //   return new Promise(function (resolve, reject) {
+// //     const user = users.find((u) => u.id === id);
+// //     // var user=users.find((user)=>{
+// //     // if(user.id==id)
+// //     // {
+// //     //     return user
+// //     // }
+// //     // })
+// //     if (user) {
+// //       resolve(user);
+// //     } else {
+// //       reject("User not found");
+// //     }
+// //   });
+// // }
+
+// // // getUserId(4)
+// // //   .then((user) => {
+// // //     console.log(user); // Output: { id: 4, name: "iti" }
+// // //   })
+// // //   .catch((err) => {
+// // // //     console.log(err);
+// // // //   });
+
+// // // //   async function execute()
+// // // //   {
+// // // //     let users= await getUserId(5); // promise
+// // // //     console.log(users);
+
+// // //   }
+
+// // //  execute()
+// // var users = [
+// //   {
+// //     id: 1,
+// //     name: "iti",
+// //   },
+// //   {
+// //     id: 2,
+// //     name: "iti",
+// //   },
+// //   {
+// //     id: 3,
+// //     name: "iti",
+// //   },
+// //   {
+// //     id: 4,
+// //     name: "iti",
+// //   },
+// // ];
+
+// // function getData(users) {
+// //   var promise = new Promise(function (resolve, reject) {
+// //     console.log(users);
+    
+// //     resolve(users);
+// //   });
+
+// //   return promise;
+// // }
+
+// // async function display() {
+// //   var test = await getData(users);
+// //   // console.log(test);
+// //  console.log(test);
+ 
+// // }
+
+// display()
+
+
+// // var users = [
+// //   { id: 1, name: "iti" },
+// // //   { id: 2, name: "iti" },
+// // //   { id: 3, name: "iti" },
+// // //   { id: 4, name: "iti" },
+// // // ];
+
+// // // function getData(users) {
+// // //   return new Promise(function (resolve, reject) {
+// // //     resolve(users);
+// // //   });
+// // // }
+
+// // // // Consume using async/await
+// // // async function displayUsers() {
+// // //   const data = await getData(users);
+// // //   console.log(data);
+// // // }
+
+// // // displayUsers();
+
+
+
+// fetch("https://dummyjson.com/products").then(
+
+//     function (response){
+//         // console.log(response);
+        
+//         return response.json() // promise
+
+//     }
+// ).then((data)=>{
+// console.log(data);
+
+// }).catch(()=>{
+//     console.log("error");
+    
 // })
 
 
-//================ history ==
-/**
- * window.history.back()
- * window.history.forward()
- * window.history.go()
- */
 
+async function execute()
+{
+  var reply= await fetch("https://dummyjson.com/products");
+  var data=await reply.json()
+  console.log(data);
+  
+
+}
+
+execute()
