98450fecc08dec267dbd18c2962bc747fa862823
ayaatadel <ayaatadel128@gmail.com>
2026-09-14 13:02:16 +0300

    day2 laravel

--- laravel/demo/routes/web.php
+++ laravel/demo/routes/web.php
@@ -1,74 +1,77 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-$users=[
-    [
-  "id"=>1,
-  "name"=>"mohammed",
-  "age"=>22
-    ],
-    [
-  "id"=>2,
-  "name"=>"mohammoud",
-  "age"=>27
-    ],
-    [
-  "id"=>3,
-  "name"=>"malak",
-  "age"=>24
-    ],
-    [
-  "id"=>4,
-  "name"=>"haneen",
-  "age"=>23
-    ],
-];
+// $users=[
+//     [
+//   "id"=>1,
+//   "name"=>"mohammed",
+//   "age"=>22
+//     ],
+//     [
+//   "id"=>2,
+//   "name"=>"mohammoud",
+//   "age"=>27
+//     ],
+//     [
+//   "id"=>3,
+//   "name"=>"malak",
+//   "age"=>24
+//     ],
+//     [
+//   "id"=>4,
+//   "name"=>"haneen",
+//   "age"=>23
+//     ],
+// ];
 
-Route::get('/users',function() use ($users)
-{
-    // var_dump($users); // blade ==> all users
-    // return view('allUsers',["users"=>$users]);
-    // compact ('var_name') ===>["users"=>$users]
-    return view('allUsers',compact('users'));
+// Route::get('/users',function() use ($users)
+// {
+//     // var_dump($users); // blade ==> all users
+//     // return view('allUsers',["users"=>$users]);
+//     // compact ('var_name') ===>["users"=>$users]
+//     return view('allUsers',compact('users'));
 
-});
-
-
-$courses=[
-    [
-  "id"=>1,
-  "name"=>"php",
-  "description"=>" sql Lorem ipsum dolor sit amet consectetur adipisicing elit."
-    ],
-    [
-  "id"=>2,
-  "name"=>"sql",
-  "description"=>" sql Lorem ipsum dolor sit amet consectetur adipisicing elit."
-
-    ],
-    [
-  "id"=>3,
-  "name"=>"laravel",
-  "description"=>" laravel Lorem ipsum dolor sit amet consectetur adipisicing elit."
-
-    ],
-    [
-  "id"=>4,
-  "name"=>"js",
-  "description"=>" js Lorem ipsum dolor sit amet consectetur adipisicing elit."
-
-    ],
-];
+// });
 
 
-Route::get('/courses',function ()use ($courses){
-return view('courses',compact('courses'));
-});
+// $courses=[
+//     [
+//   "id"=>1,
+//   "name"=>"php",
+//   "description"=>" sql Lorem ipsum dolor sit amet consectetur adipisicing elit."
+//     ],
+//     [
+//   "id"=>2,
+//   "name"=>"sql",
+//   "description"=>" sql Lorem ipsum dolor sit amet consectetur adipisicing elit."
+
+//     ],
+//     [
+//   "id"=>3,
+//   "name"=>"laravel",
+//   "description"=>" laravel Lorem ipsum dolor sit amet consectetur adipisicing elit."
+
+//     ],
+//     [
+//   "id"=>4,
+//   "name"=>"js",
+//   "description"=>" js Lorem ipsum dolor sit amet consectetur adipisicing elit."
+
+//     ],
+// ];
+
+
+// Route::get('/courses',function ()use ($courses){
+// return view('courses',compact('courses'));
+// });
 
 
 
+Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
+Route::get('/categories/{id}',[CategoryController::class,'show'])->name('categories.show');
